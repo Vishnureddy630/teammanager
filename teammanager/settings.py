73d2dbe0656ce3cf842aed78d72da8246e22df6a
M Vishnu Vardhan Reddy <117419935+Vishnureddy630@users.noreply.github.com>
2026-05-02 00:11:37 +0530

    Add logging configuration
    Add logging configuration to settings.

--- teammanager/settings.py
+++ teammanager/settings.py
@@ -134,3 +134,16 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
